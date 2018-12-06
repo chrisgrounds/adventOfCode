4bf4275029c20d16ca8c6ba79c4b5777db988da1
chris-bacon <chrisbacon2009@hotmail.com>
2018-12-06 22:54:15 +0000

    Added tests for day1

---
+++ test/Day1Spec.hs
@@ -0,0 +1,16 @@
+module Day1Spec (spec) where
+
+import Test.Hspec
+
+import Day1
+
+spec = do
+    describe "part 1" $ do
+        it "returns 3 when given [+1, +1, +1]" $ do
+            part1 ["+1", "+1", "+1"] `shouldBe` 3
+
+        it "returns 0 when given [+1, +1, -2]" $ do
+            part1 ["+1", "+1", "-2"] `shouldBe` 0
+
+        it "returns 3 when given [-1, -2, -3]" $ do
+            part1 ["-1", "-2", "-3"] `shouldBe` (-6)

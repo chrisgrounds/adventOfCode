ec68ce6adec11dbe4155778c3370bc738f3e6c49
chris-bacon <chrisbacon2009@hotmail.com>
2018-12-06 22:43:16 +0000

    Removed commented code

--- app/Main.hs
+++ app/Main.hs
@@ -8,16 +8,4 @@ main :: IO ()
 main = do
     day1
     day2
-    -- print totalChecksum
-    -- let doublesAndTriples = filter (\x -> x /= "") $ (getMultiples <$> data2) <*> ['a'..'z']
-    -- let doubles = filter (\x -> length x == 2) doublesAndTriples
-    -- let triples = filter (\x -> length x == 3) doublesAndTriples
-    -- print $ doublesAndTriples
-    -- print $ length triples
-    -- print $ length doubles * length triples
 
-    -- part 1
-    -- print $ foldr changeFrequency 0 data1
-
-    -- part 2
-    
